976f912e5ec2617cf43a05fb4d312d7282002a4e
zkk <15268588592@163.com>
2023-04-27 10:54:51 +0800

    3

--- src/Input.java
+++ src/Input.java
@@ -3,6 +3,6 @@ public class Input {
     public void testOne() throws Exception{
         int i = 0;
         int c=1/i;
-        System.out.println(c+"第一次测试");
+        System.out.println(c+"第2次测试");
     }
 }
